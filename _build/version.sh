bed6d3e755b672114fc129ed79a68a3488480631
Jörn Ott <joern.ott@ott-consult.de>
2022-04-27 15:45:14 +0200

    fix copy/paste error

--- _build/version.sh
+++ _build/version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # see https://github.com/joernott/icinga-plugin-check_ro_mounts/tags
-APP="icinga-plugin-check_iostats"
+APP="icinga-plugin-check_ro_mounts"
 VERSION="0.2.0"
 RELEASE="1"
 URL="https://github.com/joernott/icinga-plugin-check_ro_mounts/archive/refs/tags/v${VERSION}.tar.gz"
